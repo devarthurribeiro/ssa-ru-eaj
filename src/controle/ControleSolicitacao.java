df7fc015cc8302b0c28c241978493f56b64e5d2a
Arthur Ribeiro <devarthurribeiro@gmail.com>
2018-11-16 23:06:18 -0300

    Adicionado tela e controle de solicitcao

---
+++ src/controle/ControleSolicitacao.java
@@ -0,0 +1,20 @@
+package controle;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * FXML Controller class
+ *
+ * @author arthur
+ */
+public class ControleSolicitacao implements Initializable {
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+}
